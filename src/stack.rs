a040fc5415f76283a1ab8a649653360c208604ea
helloooooo <yomicerisier@gmail.com>
2018-12-09 22:29:02 +0900

    create stack for array

--- src/stack.rs
+++ src/stack.rs
@@ -1,43 +1,64 @@
+const MAX_SIZE:usize =  50;
 struct Stack {
-    stack: Vec<i64>,
-    n:i64,
+    stack:[i64;MAX_SIZE],
+    top:usize,
+    n:usize
 }
 impl Stack {
-    fn new(n:i64) -> Self {
+    fn new(n:usize) -> Self {
         Stack {
-            stack: Vec::new(),
-            n,
+            stack: [0;MAX_SIZE],
+            top: 0,
+            n
         }
     }
+    fn isEmpty(&self) -> bool {
+        self.top == 0
+    }
+    fn isFull(&self) -> bool {
+        self.top == self.n-1
+    }
     fn push(&mut self,x:i64) -> (){
-        self.stack.push(x);
+        if self.isFull() { panic!()}
+        self.top += 1;
+        self.stack[self.top] = x;
     }
     fn pop(&mut self) -> i64{
-        let v = self.stack.clone();
-        let t = v.last().unwrap();
-        self.stack.pop();
-        println!("{:?}",self.stack);
-        *t
+        if self.isEmpty() { panic!()}
+        self.top -= 1;
+        return self.stack[self.top+1];
     }
 }
 pub fn stack(input:Vec<char>,n:i64) -> i64 {
-    let mut s = Stack::new(n);
+    let mut s = Stack::new(n as usize);
     for v in input {
         if v == '+' {
             let a = s.pop();
             let b = s.pop();
+            println!("add-----------");
+            println!("a:{}",a);
+            println!("b:{}",b);
+            println!("addend-----------");
             s.push(a + b);
         } else if v == '-' {
             let a = s.pop();
             let b = s.pop();
+            println!("diff-----------");
+            println!("a:{}",a);
+            println!("b:{}",b);
+            println!("diffend-----------");
             s.push(b - a);
         } else if v == '*' {
             let a = s.pop();
             let b = s.pop();
+            println!("****-----------");
+            println!("a:{}",a);
+            println!("b:{}",b);
+            println!("****end-----------");
             s.push(a * b);
         } else {
             s.push(v.to_digit(10).unwrap() as i64);
         }
     }
-    *s.stack.last().unwrap()
+    s.stack[s.top]
 }
